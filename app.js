cd75b77621b030b79d5514d3e0003ef57c0b9b73
Allen <allenvs@mail.ru>
2020-02-21 20:32:47 +0300

    removed extra .push

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var array = [1,5,10,-2];
-var max_min_avg = [];
 
-max_min_avg.push(
+var max_min_avg = [
     Math.max(...array), 
     Math.min(...array),
     array.reduce((sum,num) => sum + num)/array.length
-); // ... is a spead operator
+]; // ... is a spead operator
 
 console.log(max_min_avg);
